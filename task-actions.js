4110aa1b05e0fe2625960bf0e0c25662a61891df
Ian Jones <ianjones834@protonmail.com>
2024-09-09 10:42:58 -0400

    fixed bug where remove tasks fails

--- task-actions.js
+++ task-actions.js
@@ -2,5 +2,5 @@ async function removeTaskFromList(listName, t) {
   const list = getList(listName);
   list.splice(t, 1);
   lists[listName] = list;
-  document.getElementById(`task-${listName}-${t}`).remove();
+  location.reload();
 }
